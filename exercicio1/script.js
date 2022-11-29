8c79c3b01156b2a15a3db56b39291b3f663a5a02
Michelle Lang Baratto <mi.baratto@gmail.com>
2022-11-29 17:12:12 +0100

    Objetos-exercício-1

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+const sacolao = []
+console.log(sacolao)
+
+const fruta1 = {
+    nome:"banana",
+    preco: 5.0,
+    disponibilidade: true
+
+}
+
+const fruta2 = {
+    nome:"maca",
+    preco: 8.0,
+    disponibilidade: true
+
+}
+
+const fruta3 = {
+    nome:"ameixa",
+    preco: 5.0,
+    disponibilidade: false
+
+}
+
+sacolao.push(fruta1, fruta2, fruta2)
+console.table(sacolao)
